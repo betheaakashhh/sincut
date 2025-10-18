e51b43642644208cb0b9d1baf7c2774bc4d18bad
Aakash kumar sahu <KARMANAM108@GMAIL.COM>
2025-10-19 01:42:14 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-inimport React from 'react'
+import React from 'react'
 import Header from '../Header/Header'
 import SlidingCards from '../Sinsection/SlidingCards'
 import Navbar from '../../components/Navbar/Navbar'
