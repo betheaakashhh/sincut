adedd29fb687f56bbb8d8c5d298d9ca67cbbcff5
Aakash kumar sahu <KARMANAM108@GMAIL.COM>
2025-10-19 04:10:51 +0530

    Update Program.jsx

--- src/pages/Program/Program.jsx
+++ src/pages/Program/Program.jsx
@@ -110,7 +110,7 @@ const Program = () => {
               Witness how your small act of giving became someone’s moment of relief..
             </p>
             <div className="cta-buttons">
-              <button className="cta-button primary" onClick={handleGalleryNavigation}>Get Started</button>
+              <button className="cta-button primary" onClick={handleGalleryNavigation}>See The Impact</button>
               <button className="cta-button secondary">Learn More</button>
             </div>
           </div>
