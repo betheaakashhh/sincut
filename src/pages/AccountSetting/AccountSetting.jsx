a55069a22f12c06da6807ebb338a592ca16840e8
Aakash sahu <sahu.aakash1008@gmail.com>
2025-11-21 22:57:51 +0530

    adding program.jsx in confess

---
+++ src/pages/AccountSetting/AccountSetting.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./account.css"
+const AccountSetting = () => {
+  return (
+    <>
+    <div className="account">
+        <h1>Account Setting Page</h1>
+        
+        </div>   
+    </>
+  )
+}
+
+export default AccountSetting
--- src/pages/Home1/Home.1.jsx
+++ src/pages/Home1/Home.1.jsx
@@ -3,11 +3,13 @@ import { Route, Routes, Navigate } from "react-router-dom";
 
 import HeaderSectionTest from "../Hero/HeroSectionTest";
 import HeroSectionWithProfile from "../ProfileHero/HeroSectionWithProfile";
+import Program from "../Program/Program";
 export const Home1 = () => {
   return (
     <div>   
         <HeroSectionWithProfile />
         <HeaderSectionTest />
+        <Program />
     </div>
   );
 }   
