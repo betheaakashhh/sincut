324f2aff9ef3bfa263f7a8119bfbf263a9beda4b
Aakash sahu <sahu.aakash1008@gmail.com>
2025-11-23 02:36:54 +0530

    changing .env in

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.REACT_APP_BACKEND_URL || 'https://sincut-razorpay.vercel.api/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://sincut-razorpay.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
