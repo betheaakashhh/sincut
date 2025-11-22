b89fcd5b6a1d9817ca1c4d64365e1e968be457ad
Aakash sahu <sahu.aakash1008@gmail.com>
2025-11-23 03:13:02 +0530

    adding and editing dashboard

--- src/pages/ProfileHero/HeroSectionWithProfile.jsx
+++ src/pages/ProfileHero/HeroSectionWithProfile.jsx
@@ -113,7 +113,7 @@ const HeroSectionWithProfile = () => {
                 
                 {showMenu && (
                   <div className="snct-dropdown-menu">
-                    <div className="snct-menu-item" onClick={() => navigate("/accountsettings")}>
+                    <div className="snct-menu-item" onClick={() => navigate("/accountsetting")}>
                       <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
                         <path d="M8 8a3 3 0 100-6 3 3 0 000 6zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
                       </svg>
